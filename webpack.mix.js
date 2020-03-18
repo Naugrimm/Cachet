2cdeb4bbc3bfae49052ee02735f1f160ac2eddd5
Erik Anders <erik.anders@securepoint.de>
2020-03-18 11:04:48 +0100

    feat: create source maps in dev

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,6 +26,7 @@ mix
         processCssUrls: false
     })
     .js('resources/assets/js/app.js', 'public/dist/js').extract(['vue', 'chart.js'])
+    .sourceMaps(false, 'inline-source-map')
     .scripts([
         'public/dist/js/app.js',
         'node_modules/es5-shim/es5-shim.js',
