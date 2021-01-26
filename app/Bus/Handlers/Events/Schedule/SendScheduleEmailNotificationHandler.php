12985112152e2e00b1dafdbfd7f65cd0d95dc89e
Simon Bockelmann <simon.bockelmann@securepoint.de>
2021-01-26 17:14:45 +0100

    added workaround for sending mails for created schedules

--- app/Bus/Handlers/Events/Schedule/SendScheduleEmailNotificationHandler.php
+++ app/Bus/Handlers/Events/Schedule/SendScheduleEmailNotificationHandler.php
@@ -12,8 +12,13 @@
 namespace CachetHQ\Cachet\Bus\Handlers\Events\Schedule;
 
 use CachetHQ\Cachet\Bus\Events\Schedule\ScheduleEventInterface;
+use CachetHQ\Cachet\Models\Setting;
 use CachetHQ\Cachet\Models\Subscriber;
 use CachetHQ\Cachet\Notifications\Schedule\NewScheduleNotification;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Mail;
+use CachetHQ\Cachet\Presenters\Traits\TimestampsTrait;
+use CachetHQ\Cachet\Services\Dates\DateFactory;
 
 /**
  * This is the send schedule event notification handler.
@@ -22,6 +27,8 @@ use CachetHQ\Cachet\Notifications\Schedule\NewScheduleNotification;
  */
 class SendScheduleEmailNotificationHandler
 {
+    use TimestampsTrait;
+
     /**
      * The subscriber instance.
      *
@@ -55,9 +62,44 @@ class SendScheduleEmailNotificationHandler
             return false;
         }
 
+        $dateFactory = new DateFactory(config('app.timezone'), Setting::where('name', '=', 'app_timezone')->first()->value);
+        $scheduleDate = ucfirst($dateFactory->make($schedule->scheduled_at)->format($this->incidentDateFormat()));
+
         // First notify all global subscribers.
-        $globalSubscribers = $this->subscriber->isVerified()->isGlobal()->get()->each(function ($subscriber) use ($schedule) {
-            $subscriber->notify(new NewScheduleNotification($schedule));
+        $globalSubscribers = $this->subscriber->isVerified()->isGlobal()->get()->each(function ($subscriber) use ($schedule, $scheduleDate) {
+            //$subscriber->notify(new NewScheduleNotification($schedule));
+            $this->subscriber = $subscriber;
+
+            $content = trans('notifications.schedule.new.mail.content', [
+                'name' => $schedule->name,
+                'date' => $scheduleDate
+            ]);
+
+            $data = [
+                'content' => $content,
+                'unsubscribe_text'        => trans('cachet.subscriber.unsubscribe'),
+                'unsubscribe_url'         => cachet_route('subscribe.unsubscribe', $subscriber->verify_code),
+                'manage_text' => trans('cachet.subscriber.manage_subscription'),
+                'manage_url'  => cachet_route('subscribe.manage', $subscriber->verify_code),
+                'year' => Carbon::now()->year,
+                'app_name' => Setting::where('name', '=', 'app_name')->first()->value,
+                'app_url' => Setting::where('name', '=', 'app_domain')->first()->value
+            ];
+
+            if (!config('mail.from.address')) {
+                $url = parse_url(env('APP_URL'));
+
+                if (isset($url['host'])) {
+                    config(['mail.from.address' => "notify@{$url['host']}"]);
+                }
+            }
+
+            Mail::send('notifications.schedule.newschedules', $data, function ($message) {
+                $message
+                    ->to($this->subscriber->email)
+                    ->subject(trans('notifications.schedule.new.mail.subject'));
+                $message->from(config('mail.from.address'), config('mail.from.name'));
+            });
         });
     }
 }
